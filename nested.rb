22b347094d54b9e2d60c13a8de77b421abb96fa2
Paul Rosero <paul_rosero@yahoo.com>
2019-04-09 23:50:48 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -73,7 +73,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-programmer_hash [][:yukihiro_matsumoto] = :known_for => "Ruby",
+programmer_hash [ ][:yukihiro_matsumoto] = :known_for => "Ruby",
 
 
 end
